987921c58b2bfaae5ab4c0ca692fcf5c89a53366
animovscw <k.s.a.basketball@gmail.com>
2025-10-24 14:35:11 +0700

    Lab1

---
+++ compressor/src/compressor.cpp
@@ -0,0 +1,41 @@
+#include <spdlog/spdlog.h>
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+
+#include "libcompressor/libcompressor.hpp"
+
+int main(int argc, char** argv) {
+  spdlog::set_level(spdlog::level::err);
+
+  if (argc < 3) {
+    spdlog::error("Usage: compressor <zlib|bzip> <string>");
+    return EXIT_FAILURE;
+  }
+
+  libcompressor_CompressionAlgorithm algo;
+  std::string a = argv[1];
+  if (a == "zlib")
+    algo = libcompressor_Zlib;
+  else if (a == "bzip")
+    algo = libcompressor_Bzip;
+  else {
+    spdlog::error("Unknown algorithm: {}", a);
+    return EXIT_FAILURE;
+  }
+
+  libcompressor_Buffer in{argv[2], (int)std::strlen(argv[2])};
+  auto out = libcompressor_compress(algo, in);
+  if (!out.data || out.size <= 0) {
+    spdlog::error("Compression failed");
+    return EXIT_FAILURE;
+  }
+
+  for (int i = 0; i < out.size; ++i) std::printf("%.2hhx", static_cast<unsigned char>(out.data[i]));
+
+  std::printf("\n");
+  std::free(out.data);
+  return EXIT_SUCCESS;
+}
